542d06a18c0aed03d496ab52f1f351d9e49bec3e
Alex <alexdodd@gmail.com>
2020-11-20 19:53:39 +0000

    second commit, with vscode, question two

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,4 +1,7 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(pet_shop_name):
-    return pet_shop_name["name"]
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
